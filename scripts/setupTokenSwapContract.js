5340bdbaf05e3cacd9b23e24a3d43cab6a0f8c2b
Richard Skeet <richard@alphacenturi.com>
2023-02-23 19:22:20 +0000

    config loader

--- scripts/setupTokenSwapContract.js
+++ scripts/setupTokenSwapContract.js
@@ -7,6 +7,7 @@ const {
 	ContractExecuteTransaction,
 	ContractCallQuery,
 	ContractFunctionParameters,
+	TokenId,
 } = require('@hashgraph/sdk');
 require('dotenv').config();
 const fs = require('fs');
@@ -22,10 +23,6 @@ const contractName = process.env.CONTRACT_NAME ?? null;
 
 const contractId = ContractId.fromString(process.env.CONTRACT_ID);
 
-const TOP_10PERCENT = 18000;
-const TOP_25PERCENT = 9000;
-const TOP_50PERCENT = 6000;
-const REMAINDER = 3000;
 const BATCH = 75;
 
 const env = process.env.ENVIRONMENT ?? null;
@@ -36,7 +33,7 @@ const main = async () => {
 	// get the command line parameters
 	const args = process.argv.slice(2);
 	if (args.length != 1 || getArgFlag('h')) {
-		console.log('usage: node setupHHB2EContract.js <file>');
+		console.log('usage: node setupTokenSwapContract.js <file>');
 		return;
 	}
 
@@ -45,10 +42,49 @@ const main = async () => {
 		return;
 	}
 
+	// read in CSV file names in args[0] and parse each line
+	try {
+		fs.access(args[0], fs.constants.F_OK, (err) => {
+			if (err) {
+				console.log(`${args[0]} does not exist`, err);
+				return;
+			}
+		});
+	}
+	catch (err) {
+		console.log(`${args[0]} does not exist`, err);
+		return;
+	}
+
+	let lineNum = 0;
+	const allFileContents = fs.readFileSync(args[0], 'utf-8');
+	const lines = allFileContents.split(/\r?\n/);
+
+	const newSerialList = [];
+	const swapHashList = [];
+	for (let l = 0; l < lines.length; l++) {
+		const line = lines[l];
+		// discard if headers [i.e. does not start with 0. for wallet ID]
+		// also remove attempts ot pay yourself
+		lineNum++;
+		if (!/^0.0.[1-9][0-9]+,/i.test(line)) {
+			console.log(`DB: Skipping line ${lineNum} - poorly formed wallet address: ${line}`);
+			continue;
+		}
+		const [token, oldSerial, newSerial] = line.split(',');
+		// casting to number may be wasted but will throw error early vs paying gas
+		newSerialList.push(Number(newSerial));
+		swapHashList.push(web3.utils.soliditySha3(
+			{ t: 'address', v: TokenId.fromString(token).toSolidityAddress() },
+			{ t: 'uint256', v: Number(oldSerial) },
+		));
+	}
+
 
 	console.log('\n-Using ENIVRONMENT:', env);
 	console.log('\n-Using Operator:', operatorId.toString());
 	console.log('\n-Using Contract:', contractId.toString());
+	console.log('\nFound config for tokens:', swapHashList.length);
 
 	if (env.toUpperCase() == 'TEST') {
 		client = Client.forTestnet();
@@ -65,55 +101,6 @@ const main = async () => {
 
 	client.setOperator(operatorId, operatorKey);
 
-	const serialList = [];
-	const earnRateList = [];
-
-	// read in the HH rank data
-	let originalJSONString;
-	// read in the file specified
-	try {
-		originalJSONString = fs.readFileSync(args[0], 'utf8');
-	}
-	catch (err) {
-		console.log(`ERROR: Could not read file (${args[0]})`, err);
-		process.exit(1);
-	}
-
-	// parse JSON
-	let combinedNFTObj;
-	try {
-		combinedNFTObj = JSON.parse(originalJSONString);
-	}
-	catch (err) {
-		console.log('ERROR: failed to parse the specified JSON', err, originalJSONString);
-		process.exit(1);
-	}
-
-	let top10p = 0, top25p = 0, top50p = 0, remainder = 0;
-	for (const n in combinedNFTObj.rarityData) {
-		const nft = combinedNFTObj.rarityData[n];
-		serialList.push(Number(nft.serial));
-		switch (nft.rankBucket) {
-		case 'top 10%':
-			earnRateList.push(TOP_10PERCENT);
-			top10p++;
-			break;
-		case 'top 25%':
-			earnRateList.push(TOP_25PERCENT);
-			top25p++;
-			break;
-		case 'top 50%':
-			earnRateList.push(TOP_50PERCENT);
-			top50p++;
-			break;
-		default:
-			earnRateList.push(REMAINDER);
-			remainder++;
-		}
-	}
-
-	console.log('Found', serialList.length, 'NFTs to setup');
-	console.log('Top 10%:', top10p, 'Top 25%:', top25p, 'Top 50%:', top50p, 'Remainder:', remainder);
 
 	// import ABI
 	const json = JSON.parse(fs.readFileSync(`./artifacts/contracts/${contractName}.sol/${contractName}.json`, 'utf8'));
@@ -122,13 +109,50 @@ const main = async () => {
 
 	const execute = readlineSync.keyInYNStrict('Do wish to push config to the contract?');
 	if (execute) {
-		for (let i = 0; i < serialList.length; i += BATCH) {
+		for (let i = 0; i < newSerialList.length; i += BATCH) {
 			console.log('Processing batch', i, '-', i + BATCH - 1);
-			const [result] = await useSetterUint256Arrays('updateSerialBurnAmount', serialList.slice(i, i + BATCH), earnRateList.slice(i, i + BATCH));
+			const [result] = await useSetterConfig('updateSwapConfig', newSerialList.slice(i, i + BATCH), swapHashList.slice(i, i + BATCH));
 			console.log('Tx', i, '-', i + BATCH, ':', result);
 		}
 	}
 };
+
+/**
+ * Generic setter caller
+ * @param {string} fcnName
+ * @param {string[]} hashes
+ * @param {number[]} serials
+ * @returns {string}
+ */
+// eslint-disable-next-line no-unused-vars
+async function useSetterConfig(fcnName, hashes, serials) {
+	const gasLim = 8000000;
+	const params = [hashes, serials];
+
+	const [setterIntArrayRx, setterResult] = await contractExecuteWithStructArgs(contractId, gasLim, fcnName, params);
+	return [setterIntArrayRx.status.toString(), setterResult];
+}
+
+
+async function contractExecuteWithStructArgs(cId, gasLim, fcnName, params, amountHbar, clientToUse = client) {
+	// use web3.eth.abi to encode the struct for sending.
+	// console.log('pre-encode:', JSON.stringify(params, null, 4));
+	const functionCallAsUint8Array = await encodeFunctionCall(fcnName, params);
+
+	const contractExecuteTx = await new ContractExecuteTransaction()
+		.setContractId(cId)
+		.setGas(gasLim)
+		.setFunctionParameters(functionCallAsUint8Array)
+		.setPayableAmount(amountHbar)
+		.freezeWith(clientToUse)
+		.execute(clientToUse);
+
+	// get the results of the function call;
+	const record = await contractExecuteTx.getRecord(clientToUse);
+	const contractResults = decodeFunctionResult(fcnName, record.contractFunctionResult.bytes);
+	const contractExecuteRx = await contractExecuteTx.getReceipt(clientToUse);
+	return [contractExecuteRx, contractResults, record];
+}
 
 function getArgFlag(arg) {
 	const customIndex = process.argv.indexOf(`-${arg}`);
